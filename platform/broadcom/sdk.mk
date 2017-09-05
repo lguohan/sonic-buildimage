dd3d2da2cb1fc26c31709c9f7ed91134bb0006f7
Guohan Lu <gulv@microsoft.com>
2017-09-05 05:43:05 +0000

    [opennsl]: use opennsl kernel module based on kernel 4.9.0-3

--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,4 +1,4 @@
-BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-5-amd64_3.4.1.11-1_amd64.deb
-$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-5-amd64_3.4.1.11-1_amd64.deb?sv=2015-04-05&sr=b&sig=jsHYNwfuoTU%2B2mhhyhC2%2FaQ%2BcaZA%2Fd21LzaaUq%2BG65c%3D&se=2031-10-17T01%3A35%3A15Z&sp=r"
+BRCM_OPENNSL_KERNEL = opennsl-modules-4.9.0-3-amd64_3.2.3.3-2_amd64.deb
+$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-4.9.0-3-amd64_3.2.3.3-2_amd64.deb?sv=2015-04-05&sr=b&sig=HieTgULvva7WlMRegUhfoRFqkj2Oe9sAVxU%2FgCGj9Ys%3D&se=2154-09-28T04%3A46%3A03Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_OPENNSL_KERNEL)
