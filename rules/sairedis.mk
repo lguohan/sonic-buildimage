54700ff41d44eb9eafb7ee6a270b17bfb9052a54
Guohan Lu <lguohan@gmail.com>
2024-09-16 11:25:39 -0700

    modify sairedis/swss to use libsaivpp
    swss should still use vs
    
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- rules/sairedis.mk
+++ rules/sairedis.mk
@@ -1,3 +1,17 @@
+# Copyright 2016 Microsoft, Inc.
+# Modifications copyright (c) 2023 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # sairedis package
 
 LIBSAIREDIS_VERSION = 1.0.0
@@ -7,7 +21,8 @@ LIBSAIREDIS = $(LIBSAIREDIS_NAME)_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
 $(LIBSAIREDIS)_SRC_PATH = $(SRC_PATH)/sonic-sairedis
 $(LIBSAIREDIS)_VERSION = $(LIBSAIREDIS_VERSION)
 $(LIBSAIREDIS)_NAME = $(LIBSAIREDIS_NAME)
-$(LIBSAIREDIS)_DEPENDS += $(LIBSWSSCOMMON_DEV)
+$(LIBSAIREDIS)_DEPENDS += $(LIBSWSSCOMMON_DEV) $(VPP_MAIN) $(VPP_PLUGIN_CORE) $(VPP_PLUGIN_DPDK) \
+	$(VPP_PLUGIN_DEV) $(VPP_DEV) $(VPPINFRA_DEV) $(VPPDBG)
 $(LIBSAIREDIS)_RDEPENDS += $(LIBSWSSCOMMON)
 $(LIBSAIREDIS)_DEB_BUILD_OPTIONS = nocheck
 SONIC_DPKG_DEBS += $(LIBSAIREDIS)
@@ -15,12 +30,16 @@ SONIC_DPKG_DEBS += $(LIBSAIREDIS)
 LIBSAIREDIS_DEV = $(LIBSAIREDIS_NAME)-dev_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIREDIS_DEV)))
 
-LIBSAIVS = libsaivs_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
-$(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIVS)))
+LIBSAIVPP = libsaivpp_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIVPP)))
+$(LIBSAIVPP)_DEPENDS += $(VPP_MAIN) $(VPP_PLUGIN_CORE) $(VPP_PLUGIN_DPDK) \
+	$(VPP_PLUGIN_DEV) $(VPP_DEV) $(VPPINFRA_DEV) $(VPPDBG)
+$(LIBSAIVPP)_RDEPENDS += $(VPP_MAIN) $(VPP_PLUGIN_CORE) $(VPP_PLUGIN_DPDK) \
+	$(VPP_PLUGIN_DEV) $(VPP_DEV) $(VPPINFRA_DEV) $(VPPDBG)
 
-LIBSAIVS_DEV = libsaivs-dev_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
-$(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIVS_DEV)))
-$(LIBSAIVS_DEV)_DEPENDS += $(LIBSAIVS)
+LIBSAIVPP_DEV = libsaivpp-dev_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIVPP_DEV)))
+$(LIBSAIVPP_DEV)_DEPENDS += $(LIBSAIVPP)
 
 LIBSAIMETADATA = libsaimetadata_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIMETADATA)))
@@ -29,20 +48,20 @@ LIBSAIMETADATA_DEV = libsaimetadata-dev_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH
 $(LIBSAIMETADATA_DEV)_DEPENDS += $(LIBSAIMETADATA)
 $(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIMETADATA_DEV)))
 
-LIBSAIREDIS_DBG = $(LIBSAIREDIS_NAME)-dbgsym_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
-$(LIBSAIREDIS_DBG)_DEPENDS += $(LIBSAIREDIS)
-$(LIBSAIREDIS_DBG)_RDEPENDS += $(LIBSAIREDIS)
-$(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIREDIS_DBG)))
+LIBSAIREDIS_DBGSYM = $(LIBSAIREDIS_NAME)-dbgsym_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
+$(LIBSAIREDIS_DBGSYM)_DEPENDS += $(LIBSAIREDIS)
+$(LIBSAIREDIS_DBGSYM)_RDEPENDS += $(LIBSAIREDIS)
+$(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIREDIS_DBGSYM)))
 
-LIBSAIVS_DBG = libsaivs-dbgsym_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
-$(LIBSAIVS_DBG)_DEPENDS += $(LIBSAIVS)
-$(LIBSAIVS_DBG)_RDEPENDS += $(LIBSAIVS)
-$(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIVS_DBG)))
+LIBSAIVPP_DBGSYM = libsaivpp-dbgsym_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
+$(LIBSAIVPP_DBGSYM)_DEPENDS += $(LIBSAIVPP)
+$(LIBSAIVPP_DBGSYM)_RDEPENDS += $(LIBSAIVPP)
+$(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIVPP_DBGSYM)))
 
-LIBSAIMETADATA_DBG = libsaimetadata-dbgsym_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
-$(LIBSAIMETADATA_DBG)_DEPENDS += $(LIBSAIMETADATA)
-$(LIBSAIMETADATA_DBG)_RDEPENDS += $(LIBSAIMETADATA)
-$(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIMETADATA_DBG)))
+LIBSAIMETADATA_DBGSYM = libsaimetadata-dbgsym_$(LIBSAIREDIS_VERSION)_$(CONFIGURED_ARCH).deb
+$(LIBSAIMETADATA_DBGSYM)_DEPENDS += $(LIBSAIMETADATA)
+$(LIBSAIMETADATA_DBGSYM)_RDEPENDS += $(LIBSAIMETADATA)
+$(eval $(call add_derived_package,$(LIBSAIREDIS),$(LIBSAIMETADATA_DBGSYM)))
 
 ifeq ($(ENABLE_PY2_MODULES), n)
     $(LIBSAIREDIS)_DEB_BUILD_PROFILES += nopython2
--- rules/swss.mk
+++ rules/swss.mk
@@ -1,12 +1,27 @@
+# Copyright 2016 Microsoft, Inc.
+# Modifications copyright (c) 2023 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # swss package
 
 SWSS = swss_1.0.0_$(CONFIGURED_ARCH).deb
 $(SWSS)_SRC_PATH = $(SRC_PATH)/sonic-swss
 $(SWSS)_DEPENDS += $(LIBSAIREDIS_DEV) $(LIBSAIMETADATA_DEV) $(LIBTEAM_DEV) \
     $(LIBTEAMDCTL) $(LIBTEAM_UTILS) $(LIBSWSSCOMMON_DEV) \
-    $(LIBSAIVS) $(LIBSAIVS_DEV) \
+    $(LIBSAIVPP) $(LIBSAIVPP_DEV) \
     $(PROTOBUF) $(PROTOBUF_LITE) $(PROTOBUF_DEV) $(LIB_SONIC_DASH_API)
-$(SWSS)_UNINSTALLS = $(LIBSAIVS_DEV)
+
+$(SWSS)_UNINSTALLS = $(LIBSAIVPP_DEV)
 
 $(SWSS)_RDEPENDS += $(LIBSAIREDIS) $(LIBSAIMETADATA) $(LIBTEAM) \
     $(LIBTEAMDCTL) $(LIBSWSSCOMMON) $(PYTHON3_SWSSCOMMON) \
